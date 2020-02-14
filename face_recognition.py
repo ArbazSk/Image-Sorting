fb871e1fca66befd61b65c15ec4c5459931f98bd
ArbazSk <ArbazSk@arbazshaikh513@gmail.com>
2020-02-14 19:14:54 +0530

    Real time

---
+++ face_recognition.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as  np
+import pickle
+
+face_cascade = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_alt.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("training_data.yml")    
+lables = {}
+with open('lables.pickle', 'rb') as f:
+    lables_ = pickle.load(f)       
+    lables = {v:k for k,v in lables_.items()}                                                                 
+
+capture = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = capture.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        roi = gray[y:y+h, x:x+w]
+
+        id_, conf = recognizer.predict(roi) 
+        if conf>50:
+            print(id_)
+            print(lables[id_])
+            cv2.putText(frame, lables[id_], (x,y), cv2.FONT_HERSHEY_PLAIN, 1, (250,255,255), 2)
+        img_ = "image.png"
+        cv2.imwrite(img_, roi)
+
+        cv2.rectangle(frame, (x,y), (x+w,y+h),(250,0,0),2)
+        
+
+    cv2.imshow('Frame', frame)
+
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+
+capture.release()
+cv2.destroyAllWindows()
